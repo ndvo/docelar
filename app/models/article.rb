9a868bfd270b1ac0d5de413160dec2b17f364aac
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-07-17 18:42:37 -0300

    Article: delete dependent comment upon article deletion

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   include Visible
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { minimum: 10 }
