66eaac089b7296eae0cb939f0f111f2e366893f2
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-07-17 10:02:24 -0300

    Article: add comments as a has many field

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  has_many :comments
+
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { minimum: 10 }
 end
