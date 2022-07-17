b90ec811db8083af15236287c57a29a56c9c76a0
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-07-17 18:48:27 -0300

    Article: add http basic authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with(
+    name: ENV['my_auth_name'] || 'admin',
+    password: ENV['my_auth_password'] || 'pass',
+    only: :destroy
+  )
+
   def index
     @articles = Article.all
   end
