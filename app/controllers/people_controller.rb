13257abac9b239afbadfc1a367a0d242040bb809
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-07-23 18:15:08 -0300

    Person: add view and controller

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,33 @@
+class PeopleController < ApplicationController
+  def index
+    Person.all
+  end
+
+  def show
+    @person = Person.find(params[:id])
+  end
+
+  def new
+    @person = Person.new
+    @countries = Country.all
+  end
+
+  def create
+    @person = Person.new(person_params)
+    if @person.save
+      redirect_to @person
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def person_params
+    params.require(:person)
+          .permit(:name, :birth,
+                  nationalities_attributes: [
+                    :country
+                  ])
+  end
+end
