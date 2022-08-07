b59b0937a8d9da2e9f0df16b786da0611e87880e
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-08-07 11:08:51 -0300

    Purchase: nested attr for payments and products
    Ideally data about products and payments should be entered in the
    purchase form. This allows the user not to think about the relationship
    between these and also not to have to create a product previous to
    creating a purchase.
    
    This still needs to improve a lot if we are to provide some intelligent
    reports about the purchases.

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,3 +1,21 @@
 class Purchase < ApplicationRecord
   belongs_to :product
+  has_many :payments
+
+  accepts_nested_attributes_for :payments,
+                                reject_if: :all_blank,
+                                allow_destroy: true
+
+  def qty_installments
+    @target_qty || payments.count
+  end
+
+  def qty_installments=(value)
+    @target_qty = value
+  end
+
+  accepts_nested_attributes_for :product
+
+  validates :product, presence: true
+  validates_associated :payments, :product
 end
