8dd1987520c4987df1de978270f231a46dad05cf
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-10-15 20:57:28 -0300

    Payment: filter by month scopes

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,14 @@
 class Payment < ApplicationRecord
   belongs_to :purchase
+
+  scope :due_at_month, ->(date) { at_month 'due_at', date }
+  scope :paid_at_month, ->(date) { at_month 'paid_at', date }
+
+  scope :at_month, (lambda do |field, date|
+    acceptable_fields = %w[paid_at due_at]
+    return where('True = False') unless acceptable_fields.include? field
+
+    month = "#{date.year}-#{date.month.to_s.rjust(2, '0')}"
+    where("strftime('%Y-%m', #{field}) = ?", month)
+  end)
 end
