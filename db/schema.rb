c5c60502b3cd791d3d5c7a7eb675b072b310ad8f
Nelson Oliveira <nelson@ocastudios.com>
2023-11-25 11:15:00 -0300

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_14_163525) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_18_142327) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -83,6 +83,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_14_163525) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "gallery_id"
+    t.index ["gallery_id"], name: "index_galleries_on_gallery_id"
   end
 
   create_table "nationalities", force: :cascade do |t|
@@ -138,7 +140,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_14_163525) do
     t.integer "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "date"
     t.string "quantity"
     t.string "integer"
     t.datetime "purchase_at"
