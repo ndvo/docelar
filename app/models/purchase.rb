ab5158c2f8715e16a83291a107a47462ae58d508
Nelson Oliveira <nelson@ocastudios.com>
2024-02-05 10:23:50 -0300

    fix: avoid error on nil price

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -14,7 +14,8 @@ class Purchase < ApplicationRecord
 
   def qty_installments=(qty)
     qty = [qty&.to_i, 1].max
-    return if prise.blank?
+    return if price.blank?
+
     installment_value = (price / qty).round(2)
 
     first_value = price - (installment_value * (qty - 1))
