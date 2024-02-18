73769b73b87bf53668e5fdb339df859d0f9014b4
Nelson Oliveira <nelson@ocastudios.com>
2024-02-18 10:48:20 -0300

    Card: add cards

---
+++ db/migrate/20240213213430_create_cards.rb
@@ -0,0 +1,18 @@
+class CreateCards < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cards do |t|
+      t.string :number
+      t.string :brand
+      t.string :name
+      t.integer :expiry_year
+      t.integer :expiry_month
+      t.integer :due_day
+      t.integer :invoice_day
+      t.decimal :limit, precision:  10, scale:  2
+
+      t.timestamps
+    end
+
+    add_reference :purchases, :card, null: true, foreign_key: true
+  end
+end
