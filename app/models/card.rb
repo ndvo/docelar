3af7429b7684daf291d5c4a5bd5f0b24494b4ae2
Nelson Oliveira <nelson@ocastudios.com>
2024-02-18 15:49:56 -0300

    Card: next due date

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,4 +4,12 @@ class Card < ApplicationRecord
   end
 
   def masked_number = "**** **** **** #{number.last(4)}"
+
+  def next_due_date_from(date)
+    due_date = Date.new(date.year, date.month, due_day)
+
+    due_date += 1.month if date.day >= invoice_day;
+    due_date += 1.month if invoice_day > due_day;
+    due_date
+  end
 end
