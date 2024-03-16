abe0abb0ba804d8c57fd4e723d25d31fdc888033
Nelson Oliveira <nelson@ocastudios.com>
2024-03-16 15:52:19 -0300

    Payment: new scopes past_month and late

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,6 +3,7 @@ class Payment < ApplicationRecord
 
   scope :due_at_month, ->(date) { at_month 'due_at', date }
   scope :paid_at_month, ->(date) { at_month 'paid_at', date }
+  scope :late, -> { past_month('due_at', Date.today).where(paid_at: nil) }
 
   scope :at_month, (lambda do |field, date|
     acceptable_fields = %w[paid_at due_at]
@@ -10,5 +11,13 @@ class Payment < ApplicationRecord
 
     month = "#{date.year}-#{date.month.to_s.rjust(2, '0')}"
     where("strftime('%Y-%m', #{field}) = ?", month)
+  end)
+
+  scope :past_month, (lambda do |field, date|
+    acceptable_fields = %w[paid_at due_at]
+    return where('True = False') unless acceptable_fields.include? field
+
+    month = "#{date.year}-#{date.month.to_s.rjust(2, '0')}"
+    where("strftime('%Y-%m', #{field}) < ?", month)
   end)
 end
