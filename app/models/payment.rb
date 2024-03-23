b151848550175eee25c764d26e8d3ebaf2d34c67
Nelson Oliveira <nelson@ocastudios.com>
2024-03-23 12:22:04 -0300

    Payment: use Datable concern

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,23 +1,9 @@
 class Payment < ApplicationRecord
   belongs_to :purchase
 
-  scope :due_at_month, ->(date) { at_month 'due_at', date }
-  scope :paid_at_month, ->(date) { at_month 'paid_at', date }
-  scope :late, -> { past_month('due_at', Date.today).where(paid_at: nil) }
+  include Datable
 
-  scope :at_month, (lambda do |field, date|
-    acceptable_fields = %w[paid_at due_at]
-    return where('True = False') unless acceptable_fields.include? field
-
-    month = "#{date.year}-#{date.month.to_s.rjust(2, '0')}"
-    where("strftime('%Y-%m', #{field}) = ?", month)
-  end)
-
-  scope :past_month, (lambda do |field, date|
-    acceptable_fields = %w[paid_at due_at]
-    return where('True = False') unless acceptable_fields.include? field
-
-    month = "#{date.year}-#{date.month.to_s.rjust(2, '0')}"
-    where("strftime('%Y-%m', #{field}) < ?", month)
-  end)
+  scope :due_at_month, ->(date) { at_month date, 'due_at' }
+  scope :paid_at_month, ->(date) { at_month date, 'paid_at' }
+  scope :late, -> { past_month(Date.today, 'due_at').where(paid_at: nil) }
 end
