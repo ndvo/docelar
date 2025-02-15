a7ac93e67d42c5071fb0f5b3ea14d59e1fc99e19
Nelson Oliveira <nelson@ocastudios.com>
2025-02-14 21:21:28 -0300

    Schema: update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_20_143155) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_14_152740) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -27,7 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_20_143155) do
     t.string "content_type"
     t.text "metadata"
     t.string "service_name", null: false
-    t.bigint "byte_size", null: false
+    t.integer "byte_size", null: false
     t.string "checksum"
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
@@ -150,10 +150,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_20_143155) do
 
   create_table "people", force: :cascade do |t|
     t.string "name"
-    t.datetime "birth"
     t.integer "nationality_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "borned_on"
+    t.date "dead_on"
     t.index ["nationality_id"], name: "index_people_on_nationality_id"
   end
 
@@ -208,16 +209,31 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_20_143155) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "responsibles", force: :cascade do |t|
+    t.integer "person_id", null: false
+    t.string "name", null: false
+    t.index ["person_id"], name: "index_responsibles_on_person_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
   create_table "tasks", force: :cascade do |t|
-    t.integer "user_id"
     t.integer "task_id"
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_completed"
+    t.integer "responsible_id"
+    t.index ["responsible_id"], name: "index_tasks_on_responsible_id"
     t.index ["task_id"], name: "index_tasks_on_task_id"
-    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "treatments", force: :cascade do |t|
@@ -229,11 +245,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_20_143155) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_hash"
-    t.string "password_salt"
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
@@ -247,6 +263,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_20_143155) do
   add_foreign_key "pharmacotherapies", "treatments"
   add_foreign_key "purchases", "cards"
   add_foreign_key "purchases", "products"
+  add_foreign_key "responsibles", "people"
+  add_foreign_key "sessions", "users"
+  add_foreign_key "tasks", "responsibles"
   add_foreign_key "tasks", "tasks"
-  add_foreign_key "tasks", "users"
 end
