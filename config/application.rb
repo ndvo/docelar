445cfa767142595b483d08233115f097876d555b
Nelson <nelson@ocastudios.com>
2026-03-28 10:28:15 -0300

    Fix Rails deprecation warnings

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,8 +21,9 @@ module Docelar
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "Brasilia"
+    config.time_zone = 'Brasilia'
     config.i18n.default_locale = :"pt-BR"
+    config.active_support.to_time_preserves_timezone = :zone
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
