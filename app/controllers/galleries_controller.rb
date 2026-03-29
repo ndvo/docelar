eafb76ae4d823696a8969acd9c5ed6193ce24f7f
Nelson <nelson@ocastudios.com>
2026-03-29 16:37:52 -0300

    fix(gallery): add pagination for photos and improve generate_photos
    - Add page-based pagination (20 photos per page)
    - Improve generate_photos with count feedback message
    - Add 'load more' button with remaining count

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -6,20 +6,26 @@ class GalleriesController < ApplicationController
 
   def show
     @gallery = Gallery.find(params[:id])
-    @photos = @gallery.photos.limit(10)
+    @page = params[:page].to_i || 1
+    per_page = 20
+    @photos = @gallery.photos.offset((@page - 1) * per_page).limit(per_page)
   end
 
-  def generate_thumbnails
-  end
+  def generate_thumbnails; end
 
   def generate_photos
-    Gallery.find(params[:id]).generate_photos
+    gallery = Gallery.find(params[:id])
+    count = gallery.photos.count
+    gallery.generate_photos
+    new_count = gallery.photos.count - count
+    flash.notice = "Geradas #{new_count} novas fotos"
+    redirect_to gallery
   end
 
   def find_new_galleries
-    new_galleries = Gallery.find_new_galleries()
+    new_galleries = Gallery.find_new_galleries
     if new_galleries.blank?
-      flash.alert = "No new galleries found"
+      flash.alert = 'No new galleries found'
     else
       flash.notice = "Found new galleries: #{new_galleries.to_sentence}"
     end
@@ -28,8 +34,7 @@ class GalleriesController < ApplicationController
 
   private
 
-  def find_pictures
-  end
+  def find_pictures; end
 
   def images_folder
     "#{Rails.root}/galleries"
