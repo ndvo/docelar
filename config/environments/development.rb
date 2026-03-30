f1eef3b557a75b604f3795dc30c0a73cab7d24d1
Nelson <nelson@ocastudios.com>
2026-03-30 20:01:15 -0300

    config(development): enable static file serving
    Serve gallery images from public directory in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Serve static files from public directory (gallery images)
+  config.public_file_server.enabled = true
+
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
