099f0e95d022db6621b3f57fbad65a763128f7b8
Nelson <nelson@ocastudios.com>
2026-04-01 10:49:00 -0300

    test(medication): add presence and case-insensitive uniqueness specs
    - Add test for name presence validation
    - Add test for case-insensitive name uniqueness

--- spec/models/medication_spec.rb
+++ spec/models/medication_spec.rb
@@ -8,5 +8,14 @@ RSpec.describe Medication, type: :model do
 
   describe 'validations' do
     it { should validate_uniqueness_of(:name) }
+    
+    it 'validates presence of name' do
+      expect(build(:medication, name: nil)).not_to be_valid
+    end
+    
+    it 'validates name uniqueness is case insensitive' do
+      create(:medication, name: 'Aspirin')
+      expect(build(:medication, name: 'Aspirin')).not_to be_valid
+    end
   end
 end
