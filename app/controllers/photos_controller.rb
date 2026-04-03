82539e206edd1d3a9d1b36597cb5e14658b23270
Nelson <nelson@ocastudios.com>
2026-04-02 22:45:07 -0300

    feat(photos): add prefetch headers for adjacent photos
    Preload adjacent photos in controller to reduce navigation latency
    when switching between photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,12 +1,22 @@
 class PhotosController < ApplicationController
   def show
-    @photo = Photo.find(params[:id])
+    @photo = Photo.includes(:gallery).find(params[:id])
     @existing_tags = TaggedPhoto
                      .includes(:tag)
                      .where.associated(:photo)
                      .where.not(photo: @photo)
                      .map(&:tag)
                      .pluck(:name).sort.uniq
+    
+    # Preload adjacent photos for smoother navigation
+    prev_photo = @photo.previous
+    next_photo = @photo.next
+    if prev_photo || next_photo
+      links = []
+      links << "<#{photo_path(prev_photo)}>; rel=prefetch" if prev_photo
+      links << "<#{photo_path(next_photo)}>; rel=prefetch" if next_photo
+      response.headers['Link'] = links.join(', ')
+    end
   end
 
   def next
