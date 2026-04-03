951b93abdde29542d6c8d43f3a6c3302b0cfca4b
Nelson <nelson@ocastudios.com>
2026-04-03 17:36:27 -0300

    feat(patients): integrate appointments on patient show page

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,6 +1,7 @@
 class Patient < ApplicationRecord
   belongs_to :individual, polymorphic: true
   has_many :treatments
+  has_many :medical_appointments
 
   validates :individual_id, uniqueness: { scope: :individual_type }
 end
