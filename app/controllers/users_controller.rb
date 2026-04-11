9e1131ee289854c6128ea2a6674d47152162cac5
Nelson <nelson@ocastudios.com>
2026-04-11 18:49:39 -0300

    fix(auth): fix SessionsController and UsersController bugs
    SessionsController:
    - Replace non-existent authenticate_by with find_by + authenticate
    
    UsersController:
    - Fix strong params to permit :email_address instead of :email
    - Fix redirect message to use @user.email_address
    - Add presence and uniqueness validations to User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_url, notice: "User #{@user.email} successfully created"
+      redirect_to root_url, notice: "User #{@user.email_address} successfully created"
     else
       render :new, status: :unprocessable_entity
     end
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email_address, :password, :password_confirmation)
   end
 end
